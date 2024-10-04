938227dfeda092f0835a40f555481eb725559e97
Mako1688 <marcoogazvega@hotmail.com>
2024-10-04 13:55:48 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,14 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//create button element
+const button = document.createElement("button");
+//set button text
+button.textContent = "💀";
+button.id = "skull-button";
+app.append(button);
+//add listening event
+button.addEventListener("click", () => {
+  alert("Button, Clicked!");
+});
